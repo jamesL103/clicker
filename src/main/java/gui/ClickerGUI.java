64e9998c4b25762223070edf883d125c740cb837
jamesL103 <jameskl7337@gmail.com>
2024-12-12 00:08:26 -0500

    Added jnativehook Added Maven build system

--- src/main/java/gui/ClickerGUI.java
+++ src/main/java/gui/ClickerGUI.java
@@ -5,6 +5,9 @@ import clicker.Clicker;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import com.github.kwhat.jnativehook.*;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
 
 /**The GUI that provides the interaction with the autoclicker
@@ -13,12 +16,15 @@ import java.awt.event.*;
 public class ClickerGUI extends JFrame {
 
     //clicker used to provide clicking input
-    private final Clicker clicker = new Clicker();
+    private final Clicker CLICKER = new Clicker();
+
+    //key to toggle clicking
+    private final int TOGGLE_KEY = NativeKeyEvent.VC_ESCAPE;
 
     //label to show clicking status
-    Label clickStatus = new Label("Autoclicker status: inactive");
+    private final JLabel CLICK_STATUS_LABEL = new JLabel("Autoclicker status: inactive");
     //label to show frequency
-    Label clickerFrequency = new Label("AutoClicker Frequency: " + clicker.getFrequency());
+    private final JLabel CLICKER_FREQ_LABEL = new JLabel("AutoClicker Frequency: " + CLICKER.getFrequency() + " clicks/s");
 
     /**Instantiates and displays the GUI for the clicker.
      *
@@ -28,34 +34,35 @@ public class ClickerGUI extends JFrame {
     }
 
     private void initGui() {
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
         BoxLayout layout = new BoxLayout(getContentPane(), BoxLayout.Y_AXIS);
         getContentPane().setLayout(layout);
-
-        setFocusable(true);
-
-        addKeyListener(new GUIKeyListener());
 
         addMouseListener(new GUIMouseListener());
 
         addFocusListener(new GUIFocusListener());
 
         setName("Auto Clicker");
-        add(new Label("Auto Clicker"));
-        add(clickStatus);
-        add(clickerFrequency);
+        add(new JLabel("Auto Clicker"));
+        add(CLICK_STATUS_LABEL);
+        add(CLICKER_FREQ_LABEL);
         initFrequencySetter();
+
+        initGlobalInput();
 
         setSize(400, 300);
         setVisible(true);
+        setFocusable(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     private void initFrequencySetter() {
-        Container frequencySetter = new Container();
-        frequencySetter.setLayout(new FlowLayout());
-        frequencySetter.add(new Label("Set Frequency: "));
+        JPanel freq_set_panel = new JPanel();
+        freq_set_panel.setLayout(new FlowLayout());
+        freq_set_panel.add(new JLabel("Set Frequency: "));
 
         JTextField frequencyField = new JTextField(); //text field to input frequency
+        frequencyField.setText(String.valueOf(CLICKER.getFrequency()));
         frequencyField.setPreferredSize(new Dimension(70, 30));
 
         JButton submit = new JButton("Confirm"); //button to confirm frequency update
@@ -64,8 +71,8 @@ public class ClickerGUI extends JFrame {
             try { //checks if there is a valid int to parse from the text field
                 int freq = Integer.parseInt(frequencyField.getText());
                 if (freq > 0) {
-                clicker.setFrequency(freq);
-                clickerFrequency.setText("AutoClicker Frequency: " + clicker.getFrequency());
+                    CLICKER.setFrequency(freq);
+                    CLICKER_FREQ_LABEL.setText("AutoClicker Frequency: " + CLICKER.getFrequency() + " clicks/s");
                 }
             } catch (NumberFormatException ignored) { //if no valid int, do nothing
 
@@ -73,40 +80,22 @@ public class ClickerGUI extends JFrame {
         });
 
         //add label and button
-        frequencySetter.add(frequencyField);
-        frequencySetter.add(submit);
+        freq_set_panel.add(frequencyField);
+        freq_set_panel.add(submit);
 
-        add(frequencySetter);
+        add(freq_set_panel);
     }
 
-    /**KeyListener for the main panel
-     * Handles the hotkeys that toggle the autoclicker.
-     *
-     */
-    private class GUIKeyListener implements KeyListener {
-        @Override
-        public void keyTyped(KeyEvent e) {
-
+    //init native hook and global input listener
+    private void initGlobalInput() {
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException e) {
+            System.out.println("error initializing native hook");
         }
-
-        @Override
-        public void keyPressed(KeyEvent e) {
-            if (e.getKeyCode() == KeyEvent.VK_A) {
-                if (clicker.isClicking()) {
-                    clicker.disableClick();
-                    clickStatus.setText("Autoclicker status: inactive");
-                } else {
-                    clicker.autoClick();
-                    clickStatus.setText("Autoclicker status: active");
-                }
-            }
-        }
-
-        @Override
-        public void keyReleased(KeyEvent e) {
-
-        }
+        GlobalScreen.addNativeKeyListener(new globalKeyListener());
     }
+
 
     /**MouseListener for the main panel
      *
@@ -154,8 +143,8 @@ public class ClickerGUI extends JFrame {
          */
         @Override
         public void focusGained(FocusEvent e) {
-            clicker.disableClick();
-            clickStatus.setText("Autoclicker status: inactive");
+            CLICKER.disableClick();
+            CLICK_STATUS_LABEL.setText("Autoclicker status: inactive");
         }
 
         @Override
@@ -164,6 +153,24 @@ public class ClickerGUI extends JFrame {
         }
     }
 
+    /**Global input listener to detect when user presses the key to
+     * toggle clicking.
+     *
+     */
+    private class globalKeyListener implements NativeKeyListener {
 
+        @Override
+        public void nativeKeyPressed(NativeKeyEvent e) {
+            if (e.getKeyCode() == TOGGLE_KEY) {
+                if (CLICKER.isClicking()) {
+                    CLICKER.disableClick();
+                    CLICK_STATUS_LABEL.setText("Autoclicker status: inactive");
+                } else {
+                    CLICKER.autoClick();
+                    CLICK_STATUS_LABEL.setText("Autoclicker status: active");
+                }
+            }
+        }
+    }
 
 }
