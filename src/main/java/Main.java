dc581ccd87ac81b40b120ad97259768161b3716c
jamesL103 <jameskl7337@gmail.com>
2025-04-12 15:09:57 -0400

    Changed look and feel of app

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,12 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
         SwingUtilities.invokeLater(() -> {
             MacroGUI gui = new MacroGUI();
         });
